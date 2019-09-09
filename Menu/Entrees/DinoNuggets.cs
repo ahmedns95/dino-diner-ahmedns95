e96d56beff622a52f94b0cefb7785c1ecd4430c9
Ahmed Alnassar <ahmedns95@users.campus>
2019-09-09 17:38:24 -0500

    first class finished

--- Menu/Entrees/DinoNuggets.cs
+++ Menu/Entrees/DinoNuggets.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace DinoDiner.Menu.Entree
 {
-    class DinoNuggets
+    public class DinoNuggets
     {
+
+        public double Price { get; set; }
+        public uint Calories { get; set; }
+
+        public List<string> Ingredients
+        {
+             get
+             {
+                  List<string> ingredients = new List<string>() { "6 chicken nuggets" };
+                  //if (bun) ingredients.Add("Whole Wheat Bun");
+                  return ingredients;
+                }
+            }
+
+        public DinoNuggets()
+        {
+             this.Price = 4.25;
+             this.Calories = 59;
+        }
+
+        //public AddAdditionalNugget()
+        //{
+
+        //}
+
+
+
     }
 }
